e2563ad06c57d8177f8af0215682d5c06df55b09
Ed Caiger <ed.caiger@gmail.com>
2025-07-15 18:12:30 +0100

    Run formatter.

--- src/logjam/cli/main.py
+++ src/logjam/cli/main.py
@@ -2,23 +2,14 @@ import argparse
 from logjam.core.filter_config import FilterConfig
 from logjam.cli.logjam_cli import LogjamCLI
 
+
 def main():
     parser = argparse.ArgumentParser(description="Logjam CLI")
     parser.add_argument(
-        "filter_config",
-        nargs="?",
-        help="Path to filter config JSON file."
+        "filter_config", nargs="?", help="Path to filter config JSON file."
     )
-    parser.add_argument(
-        "input_file",
-        nargs="?",
-        help="Path to input file."
-    )
-    parser.add_argument(
-        "output_file",
-        nargs="?",
-        help="Path to output file."
-    )
+    parser.add_argument("input_file", nargs="?", help="Path to input file.")
+    parser.add_argument("output_file", nargs="?", help="Path to output file.")
     parser.add_argument(
         "--verbose",
         "-v",
@@ -31,7 +22,7 @@ def main():
     )
     parser.add_argument(
         "--example-config",
-        help="Generate an example filter config file \"example_filter_config.json\" and exit.",
+        help='Generate an example filter config file "example_filter_config.json" and exit.',
         action="store_true",
     )
     args = parser.parse_args()
@@ -42,7 +33,9 @@ def main():
         return
 
     if not args.filter_config or not args.input_file or not args.output_file:
-        parser.error("the following arguments are required: filter_config, input_file, output_file")
+        parser.error(
+            "the following arguments are required: filter_config, input_file, output_file"
+        )
 
     logjam_cli = LogjamCLI(
         args.filter_config, args.input_file, args.output_file, args.verbose, args.filter
