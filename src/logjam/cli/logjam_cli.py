9d13e8bc107cd589d9676df6af12be12dbfa34d2
Ed Caiger <ed.caiger@gmail.com>
2025-07-15 17:57:39 +0100

    Add cli.

---
+++ src/logjam/cli/logjam_cli.py
@@ -0,0 +1,43 @@
+from logjam.core.filter_config import FilterConfig
+from logjam.core.file_filter_processor import FileFilterProcessor
+
+class LogjamCLI:
+    """Command Line Interface for Logjam."""
+
+    filter_config: FilterConfig
+    file_path: str
+    filter_processor: FileFilterProcessor
+    output_path: str | None
+    verbose: bool
+    filter_name: str | None
+
+    def __init__(
+        self,
+        filter_path: str,
+        file_path: str,
+        output_path: str | None = None,
+        verbose: bool = False,
+        filter_name: str | None = None,
+    ):
+        self.filter_config = FilterConfig.from_file(filter_path)
+        self.verbose = verbose
+        if self.verbose:
+            print(f"Loaded filter configuration from {filter_path}")
+        self.file_path = file_path
+        self.filter_processor = FileFilterProcessor(self.filter_config, self.file_path)
+        self.output_path = output_path
+        self.filter_name = filter_name
+        if not self.output_path:
+            self.output_path = "filtered_output.txt"
+
+    def run(self):
+        """Run the Logjam CLI."""
+        filtered_lines = self.filter_processor.process_filters(self.filter_name)
+        if self.verbose:
+            print(f"Found {len(filtered_lines)} matching lines.")
+        if self.output_path:
+            if self.verbose:
+                print(f"Writing filtered lines to {self.output_path}")
+            with open(self.output_path, "w") as output_file:
+                for filtered_line in filtered_lines:
+                    output_file.write(f"{filtered_line.line_content}\n")
---
+++ src/logjam/cli/main.py
@@ -0,0 +1,55 @@
+import argparse
+from logjam.core.filter_config import FilterConfig
+from logjam.cli.logjam_cli import LogjamCLI
+
+def main():
+    parser = argparse.ArgumentParser(description="Logjam CLI")
+    parser.add_argument(
+        "filter_config",
+        nargs="?",
+        help="Path to filter config JSON file."
+    )
+    parser.add_argument(
+        "input_file",
+        nargs="?",
+        help="Path to input file."
+    )
+    parser.add_argument(
+        "output_file",
+        nargs="?",
+        help="Path to output file."
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        help="Enable verbose output.",
+        default=False,
+        action="store_true",
+    )
+    parser.add_argument(
+        "--filter", "-f", help="The name of the filter to apply.", default=None
+    )
+    parser.add_argument(
+        "--example-config",
+        help="Generate an example filter config file \"example_filter_config.json\" and exit.",
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    if args.example_config:
+        FilterConfig.create_example_config().to_file("example_filter_config.json")
+        print("Example filter config written to example_filter_config.json.")
+        return
+
+    if not args.filter_config or not args.input_file or not args.output_file:
+        parser.error("the following arguments are required: filter_config, input_file, output_file")
+
+    logjam_cli = LogjamCLI(
+        args.filter_config, args.input_file, args.output_file, args.verbose, args.filter
+    )
+
+    logjam_cli.run()
+
+
+if __name__ == "__main__":
+    main()
